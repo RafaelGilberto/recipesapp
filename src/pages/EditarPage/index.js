495b1f3a4d04dca068add044c22ba88b5bf23c2b
nikolasMendes <nikolasmendes98@hotmail.com>
2023-02-14 17:59:56 -0300

    rendereização correta do receita page

---
+++ src/pages/EditarPage/index.js
@@ -0,0 +1,94 @@
+// import { useEffect, useState } from "react";
+// import { api } from "../../utils/api";
+// import { useNavigate, useParams } from "react-router-dom";
+
+// export function EditarPage() {
+
+// const navigate = useNavigate();
+// const params = useParams()
+
+//   const [receita, setReceita] = useState({
+//     name: "",
+//     description: "",
+//     imageURL: "",
+//     preparation: "",
+//     ingredients: "",
+//     status: "",
+//     rating: "☆☆☆☆☆",
+//     calories: 0,
+//     price: 0,
+//   });
+
+// useEffect(() => {
+//     async function fetchRecipe() {
+//         try {
+//             const response = await api.get(`/recipes/${params.receitaId}`)
+
+//         }
+//     }
+//     fetchRecipe()
+// },[])
+
+//   function handleChange(e) {
+//     setReceita({
+//       ...receita,
+//       [e.target.name]: e.target.value,
+//     });
+//   }
+//   async function handleSubmit(e) {
+//     try {
+//       e.preventDefault();
+
+//       const infosForAPI = { data: { ...receita } };
+//       await api.post("/recipes", infosForAPI);
+
+//       navigate("/");
+//     } catch (err) {
+//       console.log(err);
+//     }
+//   }
+//   return (
+//     <>
+//       <h1>Criar uma nova receita</h1>
+//       <form onSubmit={handleSubmit}>
+//         <label htmlFor="input-nome-receita">Nome: </label>
+//         <input
+//           id="input-nome-receita"
+//           name="name"
+//           value={receita.name}
+//           onChange={handleChange}
+//         />
+//         <label htmlFor="input-receita-descricao">Descrição: </label>
+//         <input
+//           id="input-receita-descricao"
+//           name="description"
+//           value={receita.description}
+//           onChange={handleChange}
+//         />
+//         <label htmlFor="input-imagem-receita">URL da imagem: </label>
+//         <input
+//           id="input-imagem-receita"
+//           name="imageURL"
+//           value={receita.imageURL}
+//           onChange={handleChange}
+//         />
+//         <label htmlFor="input-receita-ingredientes">Ingredientes: </label>
+//         <input
+//           htmlFor="input-receita-ingredientes"
+//           name="ingredients"
+//           value={receita.ingredients}
+//           onChange={handleChange}
+//         />
+//         <label htmlFor="input-receita-preparo">Modo de preparo: </label>
+//         <input
+//           htmlFor="input-receita-preparo"
+//           name="preparation"
+//           value={receita.preparation}
+//           onChange={handleChange}
+//         />
+
+//         <button>Criar</button>
+//       </form>
+//     </>
+//   );
+// }
