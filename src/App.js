09f4c50615618f3a6aebce1ba7a1a71b7757f95a
Rafael Gilberto Rezende Lima <rgrl516@gmail.com>
2023-02-14 15:31:31 -0300

    criando receita

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import { HomePage } from "./pages/HomePage";
+import { CriarReceita } from "./pages/CriarReceitaPage";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/criar" element={<CriarReceita />} />
       </Routes>
     </>
   );
