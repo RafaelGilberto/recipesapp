848fc52b44bcfc79566c801f7fce3ad11bba36c7
nikolasMendes <nikolasmendes98@hotmail.com>
2023-02-15 12:17:33 -0300

    fiz a base da navbar

--- src/App.js
+++ src/App.js
@@ -7,9 +7,12 @@ import { CriarReceita } from "./pages/CriarReceitaPage";
 import { ReceitaPage } from "./pages/ReceitaPage";
 import { EditarPage } from "./pages/EditarPage/index";
 
+import { NavbarRecipes } from "./Components/Navbar/index";
+
 function App() {
   return (
     <>
+      <NavbarRecipes />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/criar" element={<CriarReceita />} />
