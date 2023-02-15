7a417e36ea5659f985f7f3b1f3a01a74639dfb87
nikolasMendes <nikolasmendes98@hotmail.com>
2023-02-15 13:05:57 -0300

    testando

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import { HomePage } from "./pages/HomePage";
 import { CriarReceita } from "./pages/CriarReceitaPage";
-// import { EditarPage } from "./pages/EditarPage";
+
 import { ReceitaPage } from "./pages/ReceitaPage";
 import { EditarPage } from "./pages/EditarPage/index";
-
 import { NavbarRecipes } from "./Components/Navbar/index";
+// import { Footter } from "./Components/Footer";
 
 function App() {
   return (
@@ -16,11 +16,9 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/criar" element={<CriarReceita />} />
-        {/*
-        <Route path="/editar/:receitaId" element={<EditarPage />} />
-        */}
         <Route path="/receita/:receitaId" element={<ReceitaPage />} />
       </Routes>
+      {/* <Footter /> */}
     </>
   );
 }
